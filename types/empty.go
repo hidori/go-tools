20eb535b7800384c3cd72d456a9a5dd4a65d3c03
Hiroaki SHIBUKI <hidori@users.noreply.github.com>
2023-06-21 19:51:38 +0900

    Simplified types.Empty()

--- types/empty.go
+++ types/empty.go
@@ -1,13 +1,7 @@
 package types
 
-import "reflect"
-
 func EmptyOf[T any]() T {
 	var v T
 
-	rt := reflect.TypeOf(v)
-	rv := reflect.Zero(rt)
-	e, _ := rv.Interface().(T)
-
-	return e
+	return v
 }
